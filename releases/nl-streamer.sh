94fcd5b73a2f8d7983bf6ccc94f6d4794159c0e5
Yury Yantsevich <featalion@gmail.com>
2015-04-13 18:49:59 +0300

    fixes shell script; updates release

--- releases/nl-streamer.sh
+++ releases/nl-streamer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 os_name=$(uname -o | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m | tr '[:upper:]' '[:lower:]')
@@ -6,9 +6,11 @@ arch=$(uname -m | tr '[:upper:]' '[:lower:]')
 if [ $arch != 'x86_64' ]; then
     arch='x86'
 fi
-if [ $os_name == 'darwin' ]; then
-    os_name='macosx'
-fi    
+case $os_name in
+    'darwin') os_name='macosx';;
+    *'linux'*) os_name='linux';;
+    *) os_name='windows';;
+esac
 native_lib_path="native/$os_name/$arch"
 
 java -Djava.library.path=${native_lib_path} \
