943e2849034d7dddd8997d3fab5af0820df0fcd8
Yury Yantsevich <featalion@gmail.com>
2015-04-14 02:54:04 +0300

    bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nl-streamer "0.1.0"
+(defproject nl-streamer "0.2.0"
   :description "Stream utility to send EEG data to the Neurolyzer service."
   :url "http://efficio.cz"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,8 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [cheshire "5.4.0"]
                  [rxtx22 "1.0.6"]
-                 [clj-http-lite "0.2.1"]]
+                 [clj-http-lite "0.2.1"]
+                 [seesaw "1.4.5"]]
   :main ^:skip-aot nl-streamer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
